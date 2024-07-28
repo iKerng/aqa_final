60322c661fbc3f49ede41ea012e22f5bb3e4676b
ikerng <ikerng@gmail.com>
2024-07-28 18:18:33 +0200

    Первый коммит финального проекта по aqa с запуском с локальной машины

---
+++ conftest.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    # забираем значение параметра language из команды запуска PyTest
+    lang = request.config.getoption('language')
+    # Добавляем запуск вебдрайвера с экспериментальной функцией переключения языка от Selenium
+    chrome_options = Options()
+    chrome_options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+    # Собственные параметры запуска на локальной машине
+    chrome_driver_path = "/usr/local/bin/chromedriver"
+    chrome_options.binary_location = "/Applications/Google Chrome 114.app/Contents/MacOS/Google Chrome"
+    service = Service(chrome_driver_path)
+    print('Запускаем браузер')
+    # Для запуска теста с локальной машины
+    browser = webdriver.Chrome(service=service, options=chrome_options)
+    # browser = webdriver.Chrome()
+    yield browser
+    print('\nЗакрываем браузер')
+    # Закрываем браузер после окончания тестов
+    browser.quit()
+
+def pytest_addoption(parser):
+    # добавляем парсер для получения параметров запуска команды bash
+    parser.addoption('--language', action='store', help='Choose language', default='en')
